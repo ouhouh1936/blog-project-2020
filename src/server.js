89cdeab10ac9aeeee5f491c0927ee6be8537db0a
ouhouh1936 <ouhouh1936@gamil.com>
2020-12-05 16:30:15 +0900

    dfkajhfaifh

--- src/server.js
+++ src/server.js
@@ -1,25 +1,19 @@
-import express from "express"; // express framework를 사용하기 위해 import함
+import express from "express";
 import dotenv from "dotenv";
-dotenv.config(); // 보안 코딩
-import morgan from "morgan"; // debugging을 위해 morgan을 import함
+dotenv.config();
+import morgan from "morgan";
 import globalRouter from "./router/globalRouter";
+import path from "path";
 
 const PORT = process.env.PORT;
 
-// express를 app에 넣는다.
 const app = express();
 
-// app.js 에게 퍼그를 써야한다고 신호를 줌
-// pug <- 키워드
 app.set("view engine", "pug");
-
-// app에게 morgan를 써야한다고 신호를 준다.
-// dev <- 키워드
 app.use(morgan(`dev`));
-
+app.use(express.static(path.join(__dirname, `assets`)));
 app.use("/", globalRouter);
 
-// 설정 끝난 후 Server Start
 app.listen(PORT, () => {
   console.log(`✅  ${PORT} Server Start`);
 });
